308311964c07b6bb61656581ff7bc4306c132853
Francisco Castillo <cesarfranciscocastillo@gmail.com>
2021-11-26 18:45:00 -0300

    commit inicial

---
+++ src/test/java/com/demo/cloud/CloudConfigAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudConfigAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
